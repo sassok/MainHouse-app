0e03cf92579a28210ad94a8bdddcdc8f75097f14
anthopayan <anthony.payan08@outlook.fr>
2021-06-17 13:08:26 +0200

    Start der Landingpage Erstellung

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './style.css';
+import logo from  '../../assets/images/Mainhouseblack.png'
 
 const Navbar = () => {
     return(
         <div>
             <div className="containerNavbar">
+                <p className="logoContainer">
+                    <img className="logo" alt="logo" src={logo}></img>
+                </p>
                 <button  className="componentsNavbarLogin">Se connecter</button>
                 <button  className="componentsNavbarSignup">S'inscrire</button>
             </div>
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import Landingpage from './pages/Landingpage/index'
 
 const App = () => {
     return (
-        <h1>
+        <>
             <Landingpage/>
-        </h1>
+        </>
     );
   };
   ReactDOM.render(<App />, document.getElementById('root'));
