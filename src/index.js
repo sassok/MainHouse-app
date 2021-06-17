b51c559181e2048e34f9b3ebc9aa50b028709104
volpito <mpj.rigot@gmail.com>
2021-06-17 11:32:39 +0200

    sidebar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Landingpage from './pages/Landingpage/index'
+import Sidebar from './components/Sidebar';
 
 const App = () => {
     return (
-        <h1>
+        <>
+            <Sidebar/>
             <Landingpage/>
-        </h1>
+            <div id="conteneur">
+                <div className="row1"> 
+                </div>
+                <div className="row2">
+                  <h1>bonjour</h1> 
+                </div>
+            </div>
+        </>
     );
   };
   ReactDOM.render(<App />, document.getElementById('root'));
