24c685d9b476e9ef703249b6f0fd58f6ae5cc2dd
sassok <kevin.sassoo@gmail.com>
2021-06-17 09:53:28 +0200

    add footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h3>Besoin d’aide ? Contactez nous à hello@mkdev.com</h3>
+      <p className="copyrights">Mk Dev corporation | 2021 © Tous droits réservés</p>
+    </footer>
+  );
+};
+
+export default Footer;
