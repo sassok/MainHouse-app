b9b8838beaa7b440361125cea71650329280d30b
sassok <kevin.sassoo@gmail.com>
2021-06-18 09:19:47 +0200

    fetch agency-buildings work

--- src/components/IndexBuilding/index.jsx
+++ src/components/IndexBuilding/index.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import './style.css';
 import Cookies from 'js-cookie';
-import { useSelector } from 'react-redux';
 
 const IndexBuilding = () => {
+    const agency = useSelector((state) => state.agency);
     const [building, setBuilding] = useState([]);
     const id = useSelector(state => state.agency.id);
+    const bearer = useSelector(token => token.bearer)
 
     useEffect (() => {
-      const fetchArticle = async () => {
+      const fetchBuilding = async () => {
       fetch(`https://mainhouseapi.herokuapp.com/agency-buildings/${id}` , {
         method: 'get',
         headers: {
@@ -17,18 +20,27 @@ const IndexBuilding = () => {
         },
         }).then((response) => response.json())
           .then((response) => {
-            setBuilding(response);
+            setBuilding(response.buildings);
             console.log(response)
             }).catch(function() {
               console.log("error");
         });
   
-      }; 
-  
-      fetchArticle()
+      };       
+
+      fetchBuilding()
       }, []);
+
   return (  
-  <h1>BONJOUR MAMAN</h1>
+  <div>
+    {building.map(build => (
+      <div>
+      <p>{build.city}</p>
+      <p>{build.name}</p>
+      <p>{build.reference}</p>
+      </div>
+    ))}
+  </div>
   );
 };
 
