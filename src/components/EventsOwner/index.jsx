6e5cfb67ac16b10c6b36c631f619f4c51469e743
anthopayan <anthony.payan08@outlook.fr>
2021-06-25 10:56:14 +0200

    events owner

--- src/components/EventsOwner/index.jsx
+++ src/components/EventsOwner/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import closeButton from '../../assets/images/iconeCloseButton.png';
 
 import Cookies from 'js-cookie';
 import party from "../../assets/images/party.jpeg";
@@ -10,6 +11,16 @@ const EventsOwner = () => {
     const [event, setEvent] = useState([]);
     const id = useSelector(state => state.owner.building_id);
     const bearer = useSelector(token => token.bearer)
+    const [Eventid, setEventId] = useState([]);
+
+    const setDisplay = () => {
+      document.getElementsByClassName('aside-right')[0].style.display = "block";
+      document.getElementsByClassName('showownereventright')[0].style.display = "block";
+    }
+
+    const setDisplayNone = () => {
+      document.getElementsByClassName('aside-right')[0].style.display = "none";
+    }
 
     useEffect (() => {
       const fetchEvent = async () => {
@@ -30,12 +41,69 @@ const EventsOwner = () => {
       fetchEvent()
       }, []);
 
+      const ShowEventOwner = (props) => {
+        const [singleevent, setOneEvent] = useState([]);
+    
+        useEffect (() => {
+          const fetchShowEvent = async () => {
+          fetch(`https://mainhouseapi.herokuapp.com/events/${props.id}` , {
+            method: 'get',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            }).then((response) => response.json())
+              .then((response) => {
+                setOneEvent(response);
+                }).catch(function() {
+                  console.log("error");
+            });
+          };       
+    
+          fetchShowEvent()
+          
+          }, [props]);
+    
+        return (
+          <div className="showbuildingcard">
+        <div className="containerbuilding-show">
+          <div className="card-building-show">
+            <div className="card-building-header-show">
+              <img className="card-building-img-show " src="http://www.riadmehdi.net/wp-content/uploads/2018/11/csm_img-event_54745635d1.jpg" alt="rover" />
+            </div>
+            <div className="card-building-body-show">
+              <div className="cardbuildname-show">
+                <p>{singleevent.title}</p>
+              </div>
+              <span className="cardbuildref-show">Description: {singleevent.description}</span>
+              <span className="cardbuildref-show">Le: {singleevent.datetime}</span>
+              <span className="cardbuildref-show mb-5">Durée: {singleevent.duration} minutes</span>
+              <img className="iconeCloseButton" src={closeButton} alt="closebutton" onClick={() => setDisplayNone()}></img>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
+
+    
+
+ 
+        );
+      }
+
+      const getId = (id) => {
+        setEventId(id);
+        setDisplay();
+      }
+
   return ( 
+    <>
     <main className="main">
       <div className="eventlistcards">
         <div className="listevents">
           {event.map(events => (
-            <div className="list-item-event" key="events.id">      
+            <div className="list-item-event" key="events.id" onClick={() => getId(events.id)}>      
               <p className="event-list-title">{events.title}</p>
               <p className="event-list-date">{events.datetime}</p>
               <p className="event-list-description">{events.description}</p>    
@@ -44,6 +112,12 @@ const EventsOwner = () => {
         </div>
       </div>
     </main>
+     <aside className="aside-right showownereventright">
+     <div className="showownereventright">
+     <ShowEventOwner id={Eventid}/>
+     </div>
+     </aside>
+     </>
   );
 };
 
